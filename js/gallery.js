7fc451088fbc7a6e6c9cf563c60324759d992f68
Sumin <ghkdtnals729@gmail.com>
2026-01-09 23:55:53 +0900

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -4,43 +4,43 @@ $(document).ready(function () {
     const currMonth = urlParams.get('month');
     const currFolder = urlParams.get('id');
 
-    $.getJSON("data.json", function (configData) {
-        const cfg = configData.config;
+    $.getJSON("data.json", function (data) {
+        const cfg = data.config;
         let html = "";
         let titleHtml = `<a href="gallery.html">Home</a> / <a href="gallery.html">Gallery</a> / `;
         const $container = $("#gallery_container");
 
-        // 초기화: 모든 특수 열 클래스 제거
+        // [초기화] 서브페이지(4열) 클래스 제거
         $container.removeClass("sub_mode");
 
-        // [STEP 1] 연도만 있을 때: 1월 ~ 12월 리스트 출력 (5열 그리드)
+        // 1단계: 특정 연도 클릭 시 -> 1월~12월 리스트 (기본 5열)
         if (currYear && !currMonth) {
             titleHtml += `<span>${currYear}</span>`;
-
-            cfg.months.forEach(m => {
-                // 임시 썸네일 경로 (실제 파일이 없을 경우 대비하여 첫 번째 폴더의 첫 번째 이미지 지정)
+            for (let i = 1; i <= 12; i++) {
+                let m = i.toString().padStart(2, '0');
+                // 모든 월을 활성화 상태로 만듦
                 let thumbImg = `images/${currYear}/${m}/folder_1/01.jpg`;
                 html += `
                     <div class="archive_item">
                         <a href="gallery_sub.html?year=${currYear}&month=${m}">
                             <div class="thumb_box" style="background-image:url('${thumbImg}')"></div>
-                            <p class="year_text">${parseInt(m)}월</p>
+                            <p class="year_text">${i}월</p>
                         </a>
                     </div>`;
-            });
+            }
         }
 
-        // [STEP 2] 월까지 있을 때: 임시 폴더 9개 출력 (4열 그리드)
+        // 2단계: 특정 월 클릭 시 -> 임시 폴더 9개 (4열 모드)
         else if (currYear && currMonth && !currFolder) {
-            $container.addClass("sub_mode"); // 4열 모드 활성화
+            $container.addClass("sub_mode");
             titleHtml += `<a href="gallery_sub.html?year=${currYear}">${currYear}</a> / <span>${parseInt(currMonth)}월</span>`;
 
-            cfg.folder_names.forEach((name, index) => {
-                let folderId = `folder_${index + 1}`;
-                let thumbImg = `images/${currYear}/${currMonth}/${folderId}/01.jpg`;
+            cfg.folderNames.forEach((name, index) => {
+                let fId = `folder_${index + 1}`;
+                let thumbImg = `images/${currYear}/${currMonth}/${fId}/01.jpg`;
                 html += `
                     <div class="archive_item">
-                        <a href="gallery_sub.html?year=${currYear}&month=${currMonth}&id=${folderId}">
+                        <a href="gallery_sub.html?year=${currYear}&month=${currMonth}&id=${fId}">
                             <div class="thumb_box" style="background-image:url('${thumbImg}')"></div>
                             <p class="year_text">${name}</p>
                         </a>
@@ -48,18 +48,18 @@ $(document).ready(function () {
             });
         }
 
-        // [STEP 3] 폴더(ID)까지 있을 때: 이미지 01.jpg ~ 09.jpg 출력 (4열 그리드)
+        // 3단계: 특정 폴더 클릭 시 -> 01.jpg~09.jpg (4열 모드)
         else if (currFolder) {
             $container.addClass("sub_mode");
-            let folderIdx = parseInt(currFolder.split('_')[1]) - 1;
-            let folderName = cfg.folder_names[folderIdx];
+            let fIdx = parseInt(currFolder.split('_')[1]) - 1;
+            let fName = cfg.folderNames[fIdx];
 
             titleHtml += `
                 <a href="gallery_sub.html?year=${currYear}">${currYear}</a> / 
                 <a href="gallery_sub.html?year=${currYear}&month=${currMonth}">${parseInt(currMonth)}월</a> / 
-                <span>${folderName}</span>`;
+                <span>${fName}</span>`;
 
-            cfg.image_list.forEach(imgName => {
+            cfg.images.forEach(imgName => {
                 let path = `images/${currYear}/${currMonth}/${currFolder}/${imgName}`;
                 html += `
                     <div class="archive_item">
@@ -73,7 +73,7 @@ $(document).ready(function () {
         $container.html(html);
     });
 
-    // 라이트박스 및 기타 기능
+    // 라이트박스 & 상단이동
     $(document).on('click', '.lightbox_trigger', function () {
         $('#lightbox img').attr('src', $(this).data('src'));
         $('#lightbox').css('display', 'flex');

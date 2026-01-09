bf28c59c223b99b60ff3132694b8d05182535e24
Sumin <ghkdtnals729@gmail.com>
2026-01-10 00:21:38 +0900

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -7,76 +7,66 @@ $(document).ready(function () {
     $.getJSON("data.json", function (data) {
         const cfg = data.config;
         let html = "";
-        let titleHtml = `<a href="gallery.html">Home</a> / <a href="gallery.html">Gallery</a> / `;
+        let titleHtml = `<a href="gallery.html" style="display:inline;">Home</a> / <a href="gallery.html" style="display:inline;">Gallery</a> / `;
         const $container = $("#gallery_container");
 
-        // [초기화] 서브페이지(4열) 클래스 제거
+        // [핵심] 페이지 로드 시 그리드 모드 초기화
         $container.removeClass("sub_mode");
 
-        // 1단계: 특정 연도 클릭 시 -> 1월~12월 리스트 (기본 5열)
         if (currYear && !currMonth) {
+            // 1. 연도 선택 페이지 -> 5열 유지
             titleHtml += `<span>${currYear}</span>`;
             for (let i = 1; i <= 12; i++) {
                 let m = i.toString().padStart(2, '0');
-                // 모든 월을 활성화 상태로 만듦
-                let thumbImg = `images/${currYear}/${m}/folder_1/01.jpg`;
                 html += `
                     <div class="archive_item">
                         <a href="gallery_sub.html?year=${currYear}&month=${m}">
-                            <div class="thumb_box" style="background-image:url('${thumbImg}')"></div>
+                            <div class="thumb_box"></div>
                             <p class="year_text">${i}월</p>
                         </a>
                     </div>`;
             }
         }
-
-        // 2단계: 특정 월 클릭 시 -> 임시 폴더 9개 (4열 모드)
-        else if (currYear && currMonth && !currFolder) {
+        else if (currYear && currMonth) {
+            // 2. 월 선택 또는 상세 페이지 -> 4열 모드(sub_mode) 활성화
             $container.addClass("sub_mode");
-            titleHtml += `<a href="gallery_sub.html?year=${currYear}">${currYear}</a> / <span>${parseInt(currMonth)}월</span>`;
 
-            cfg.folderNames.forEach((name, index) => {
-                let fId = `folder_${index + 1}`;
-                let thumbImg = `images/${currYear}/${currMonth}/${fId}/01.jpg`;
-                html += `
-                    <div class="archive_item">
-                        <a href="gallery_sub.html?year=${currYear}&month=${currMonth}&id=${fId}">
-                            <div class="thumb_box" style="background-image:url('${thumbImg}')"></div>
-                            <p class="year_text">${name}</p>
-                        </a>
-                    </div>`;
-            });
+            if (!currFolder) {
+                // 월 클릭 시 (폴더 리스트)
+                titleHtml += `<a href="gallery_sub.html?year=${currYear}" style="display:inline;">${currYear}</a> / <span>${parseInt(currMonth)}월</span>`;
+                cfg.folderNames.forEach((name, index) => {
+                    let fId = `folder_${index + 1}`;
+                    html += `
+                        <div class="archive_item">
+                            <a href="gallery_sub.html?year=${currYear}&month=${currMonth}&id=${fId}">
+                                <div class="thumb_box"></div>
+                                <p class="year_text">${name}</p>
+                            </a>
+                        </div>`;
+                });
+            } else {
+                // 폴더 클릭 시 (상세 이미지)
+                titleHtml += `<a href="gallery_sub.html?year=${currYear}" style="display:inline;">${currYear}</a> / <a href="gallery_sub.html?year=${currYear}&month=${currMonth}" style="display:inline;">${parseInt(currMonth)}월</a> / <span>Detail</span>`;
+                cfg.images.forEach(img => {
+                    html += `
+                        <div class="archive_item">
+                            <div class="thumb_box lightbox_trigger"></div>
+                            <p class="year_text">${img}</p>
+                        </div>`;
+                });
+            }
         }
-
-        // 3단계: 특정 폴더 클릭 시 -> 01.jpg~09.jpg (4열 모드)
-        else if (currFolder) {
-            $container.addClass("sub_mode");
-            let fIdx = parseInt(currFolder.split('_')[1]) - 1;
-            let fName = cfg.folderNames[fIdx];
-
-            titleHtml += `
-                <a href="gallery_sub.html?year=${currYear}">${currYear}</a> / 
-                <a href="gallery_sub.html?year=${currYear}&month=${currMonth}">${parseInt(currMonth)}월</a> / 
-                <span>${fName}</span>`;
-
-            cfg.images.forEach(imgName => {
-                let path = `images/${currYear}/${currMonth}/${currFolder}/${imgName}`;
-                html += `
-                    <div class="archive_item">
-                        <div class="thumb_box lightbox_trigger" data-src="${path}" style="background-image:url('${path}')"></div>
-                        <p class="year_text">${imgName.split('.')[0]}</p>
-                    </div>`;
-            });
-        }
-
         $("#dynamic_title").html(titleHtml);
         $container.html(html);
     });
 
-    // 라이트박스 & 상단이동
-    $(document).on('click', '.lightbox_trigger', function () {
-        $('#lightbox img').attr('src', $(this).data('src'));
-        $('#lightbox').css('display', 'flex');
+    // TOP 버튼 동작
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) $('#top_btn').fadeIn();
+        else $('#top_btn').fadeOut();
     });
-    $('#lightbox, .close_btn').click(function () { $('#lightbox').hide(); });
+    $('#top_btn').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 400);
+    });
 });
